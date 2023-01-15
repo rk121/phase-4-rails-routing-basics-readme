fd9222a4ed7be27219c39393a8d6a581dedab6b4
rk121 <roshankhadka613@gmail.com>
2023-01-15 17:51:01 +1100

    commit tree 0a25e76d8e3199ce54517f0202e483ebd243de59

---
+++ app/controllers/cheeses_controller.rb
@@ -0,0 +1,11 @@
+class CheesesController < ApplicationController
+    def index 
+        cheeses = Cheese.all
+        render json: cheeses
+    end
+
+    def show
+        cheese = Cheese.find_by(id: params[:id])
+        render json: cheese
+    end
+end
